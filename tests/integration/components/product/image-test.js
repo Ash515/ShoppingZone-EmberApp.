0f13c901015ceb22cd9f2b26a5f201ad966e3fb1
Ash515 <aaashwin515@gmail.com>
2022-01-15 18:22:50 +0530

    Added new codes

--- tests/integration/components/product/image-test.js
+++ tests/integration/components/product/image-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | product', function (hooks) {
+module('Integration | Component | product/image', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<Product />`);
+    await render(hbs`<Product::Image />`);
 
     assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
-      <Product>
+      <Product::Image>
         template block text
-      </Product>
+      </Product::Image>
     `);
 
     assert.dom(this.element).hasText('template block text');
