16fa11294cd163da34ab7a55cba5cb1ae91fe10b
Ash515 <aaashwin515@gmail.com>
2022-01-16 21:24:23 +0530

    Added new codes

--- app/controllers/item.js
+++ app/controllers/item.js
@@ -1,3 +1,12 @@
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import {action} from '@ember/object';
+export default class ItemController extends Controller {
 
-export default class ItemController extends Controller {}
+@tracked color="red";
+
+@action
+onChangeColor(newcolor){  //after user clicking the color input is passed as a arg in this action function
+    this.color=newcolor;
+}
+}
