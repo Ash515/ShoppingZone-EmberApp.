cc6c9d1da48bc4d2267c179a6d97ab76052dc395
Ash515 <aaashwin515@gmail.com>
2022-01-16 19:58:30 +0530

    Added new codes

--- app/components/product.js
+++ app/components/product.js
@@ -1,3 +1,5 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class ProductComponent extends Component {}
--- app/controllers/cart.js
+++ app/controllers/cart.js
@@ -2,8 +2,8 @@ import Controller from '@ember/controller';
 
 export default class CartController extends Controller {
   get subtotal() {
-    return this.model.reduce((acc, item) => {
-      return acc + item.price;
+    return this.model.reduce((accumulator, curr_item) => {
+      return accumulator + curr_item.price;
     }, 0);
   }
   get tax() {
